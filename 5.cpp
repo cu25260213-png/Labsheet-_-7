86f7cfba87a11e9e77140f0f1a992650494c5df2
cu25260213-png <cu25260213@coeruniversity.ac.in>
2025-11-27 12:50:04 +0530

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,17 @@
+include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char s1[100], s2[100];
+
+    cout << "Enter first string: ";
+    cin.getline(s1, 100);
+
+    cout << "Enter second string: ";
+    cin.getline(s2, 100);
+
+    strcat(s1, s2);
+
+    cout << "Concatenated: " << s1;
+}
