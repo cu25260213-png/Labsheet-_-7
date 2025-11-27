acf0f55c1491fac3c51c833e121b90417f526d0c
cu25260213-png <cu25260213@coeruniversity.ac.in>
2025-11-27 12:53:02 +0530

    Create 8.cpp

---
+++ 8.cpp
@@ -0,0 +1,17 @@
+include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    char s1[100], s2[100];
+
+    cout << "Enter first string: ";
+    cin.getline(s1, 100);
+
+    cout << "Enter second string: ";
+    cin.getline(s2, 100);
+
+    if(strcmp(s1, s2) == 0)
+        cout << "Equal (case-sensitive)";
+    else
+        cout << "Not equal";
